aef8de6aa77e4f206affbd48355adf274d545d0b
xiayulu <zuiaiqiansen@163.com>
2021-09-19 22:06:13 +0800

    block parser finished

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use std::collections::HashMap;
+
 #[derive(Debug)]
 struct Mark(String);
 
@@ -21,7 +23,137 @@ impl Parser {
     }
 
     pub fn parse(&mut self, input: &String) {
-        self.marks.push(Mark(String::from("**")));
+        let mut mark = Vec::new();
+        let mut start = 0usize;
+        let mut pre = 0usize;
+        let mut inline = false;
+        for (idx, char) in input.char_indices() {
+            match char {
+                '#' => {
+                    if !inline || idx - pre == 1 {
+                        println!("Head start...");
+                        mark.push(char);
+                        pre = idx;
+                    }
+                }
+                '`' => {
+                    if idx - pre == 1 {
+                        // may start with
+                        println!("Get `");
+                        mark.push(char);
+                    }
+                    pre = idx;
+                }
+                '$' => {
+                    if idx - pre == 1 {
+                        // may start with
+                        println!("Get $");
+                        mark.push(char);
+                    }
+                    pre = idx;
+                }
+                '>' => {
+                    if !inline {
+                        println!("pre start...");
+                        mark.push(char);
+                    }
+                }
+                '-' => {
+                    if !inline {
+                        println!("List start...");
+                        mark.push(char);
+                    }
+                }
+                '|' => {
+                    println!("table")
+                }
+                '\n' => {
+                    if !mark.is_empty() {
+                        match mark[0] {
+                            '#' => {
+                                /* header
+                                 * start with 1-6 '#',
+                                 * end with 'newline'('\r\n')
+                                 */
+                                let end = idx + char.len_utf8();
+                                println!("Get head {:?}", &input[start..end]);
+                                self.blocks.push(Block(String::from(&input[start..end])));
+                                start = end;
+                                mark.clear();
+                                inline = false;
+                            }
+                            '-' => {
+                                /** list
+                                 * start with '-'
+                                 * end with 'newline'('\r\n')
+                                 */
+                                let end = idx + char.len_utf8();
+                                println!("Get list {:?}", &input[start..end]);
+                                self.blocks.push(Block(String::from(&input[start..end])));
+                                start = end;
+                                mark.clear();
+                                inline = false;
+                            }
+                            '>' => {
+                                /** Pre
+                                 * start with '>'
+                                 * end with 'newline'('\r\n')
+                                 */
+                                let end = idx + char.len_utf8();
+                                println!("Get Pre {:?}", &input[start..end]);
+                                self.blocks.push(Block(String::from(&input[start..end])));
+                                start = end;
+                                mark.clear();
+                                inline = false;
+                            }
+                            '`' => {
+                                /** fenced code
+                                 * start with '```'
+                                 * end with '```newline'('\r\n')
+                                 */
+                                if mark.len() >= 4 {
+                                    let end = idx + char.len_utf8();
+                                    println!("Get Fenced Code {:?}", &input[start..end]);
+                                    self.blocks.push(Block(String::from(&input[start..end])));
+                                    start = end;
+                                    mark.clear();
+                                    inline = false;
+                                }
+                            }
+                            '$' => {
+                                /** Display math
+                                 * start with '$$'
+                                 * end with '$$newline'('\r\n')
+                                 */
+                                if mark.len() >= 2 {
+                                    let end = idx + char.len_utf8();
+                                    println!("Get display math {:?}", &input[start..end]);
+                                    self.blocks.push(Block(String::from(&input[start..end])));
+                                    start = end;
+                                    mark.clear();
+                                    inline = false;
+                                }
+                            }
+                            _ => (),
+                        }
+                    } else {
+                        /* paragraph
+                         * start with '',
+                         * end with 'newline'('\r\n')
+                         */
+                        let end = idx + char.len_utf8();
+                        println!("Get paragraph {:?}", &input[start..end]);
+                        self.blocks.push(Block(String::from(&input[start..end])));
+                        start = end;
+                        mark.clear();
+                        inline = false;
+                    }
+                }
+                _ => {
+                    inline = true;
+                }
+            }
+        }
     }
 
     pub fn show(&self) {
@@ -41,10 +173,7 @@ mod tests {
     #[test]
     fn it_worked() {
         // run with cargo test -- --nocapture
-        let input = fs::read_to_string("tests/test.md").unwrap();
-        println!("{:?}", input);
-        print_type_of(&input);
-
+        let input = fs::read_to_string("tests/test3.md").unwrap();
         let mut parser: Parser = Parser::new();
         parser.parse(&input);
         parser.show();
