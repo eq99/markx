bc6c4b4d8b8f4d7b037e5cf2866ba7a5e42289ed
xiayulu <zuiaiqiansen@163.com>
2021-09-28 23:20:55 +0800

    refator lexer

--- src/block.rs
+++ src/block.rs
@@ -9,49 +9,92 @@ struct Mark(String);
 struct Block(String);
 
 #[derive(Debug)]
-pub struct Parser {
+pub struct BlockParser {
     marks: Vec<Mark>,
     blocks: Vec<Block>,
 }
 
-impl Parser {
+impl BlockParser {
     pub fn new() -> Self {
-        Parser {
+        BlockParser {
             marks: Vec::new(),
             blocks: Vec::new(),
         }
     }
 
     pub fn parse(&mut self, input: &String) {
-        let mut is_open_tag = true;
-        let mut muti_line = String::from("");
+        let mut open_tag = String::from("");
+        let mut multi_line = String::from("");
         for line in input.lines() {
             if line.starts_with("#") {
+                // Get the head block.
                 println!("Get HEAD: {}", line);
-            } else if line.starts_with(">") {
-                println!("Get PRE: {}", line);
             } else if line.starts_with("-") {
+                // Get list item.
                 println!("Get LIST: {}", line);
             } else if line.starts_with("|") {
+                // Get table row
                 println!("Get ROW: {}", line);
             } else if line.starts_with("```") {
-                if is_open_tag {
-                    is_open_tag = false;
-                    muti_line += line;
-                    muti_line += "\n";
+                // Get fenced code block.
+                // All lines will be gathered in the last `else` statement.
+                // You can review the logic near `Paragraph Block`
+                //
+                // One thing need to note:
+                // This can be a normal code block or
+                // a block in `Pre Block, Exercise Block.`
+
+                if open_tag.is_empty() {
+                    // normal fenced code starts
+                    open_tag += "```";
+                    multi_line += line.trim_start_matches('`');
+                    multi_line += "\n";
+                } else if open_tag.starts_with("`") {
+                    // normal fenced code ends
+                    println!("Get CODE: {}", multi_line);
+                    open_tag.clear();
+                    multi_line.clear();
                 } else {
-                    println!("Get CODE: {}", muti_line + line);
-                    is_open_tag = true;
-                    muti_line = String::from("");
+                    // fenced code in some other block.
+                    multi_line += line;
+                    multi_line += "\n";
                 }
             } else if line.starts_with("$$") {
-                println!("Get MATH: {}", line);
+                // The logic is same as fenced code
+                if open_tag.is_empty() {
+                    // display math starts.
+                    open_tag += "$$";
+                } else if open_tag.starts_with("$") {
+                    // display math ends.
+                    println!("Get Math: {}", multi_line);
+                    open_tag.clear();
+                    multi_line.clear();
+                } else {
+                    // display math in other block.
+                    multi_line += line;
+                    multi_line += "\n";
+                }
+            } else if line.starts_with("%%") {
+                // `Pre Block` can not be contained in other block.
+                if open_tag.is_empty() {
+                    // pre block starts.
+                    open_tag += "%%";
+                    multi_line += line.trim_start_matches('%');
+                    multi_line += "\n";
+                } else {
+                    // end of pre block.
+                    println!("Get Pre: {}", multi_line);
+                    open_tag.clear();
+                    multi_line.clear();
+                }
             } else {
-                if is_open_tag {
+                if open_tag.is_empty() {
+                    // All normal line will be regarded as paragraph.
                     println!("Get P: {}", line);
                 } else {
-                    muti_line += line;
-                    muti_line += "\n";
+                    // These lines are in some multi-line blocks.
+                    multi_line += line;
+                    multi_line += "\n";
                 }
             }
         }
@@ -63,7 +106,7 @@ impl Parser {
 }
 #[cfg(test)]
 mod tests {
-    use super::Parser;
+    use super::BlockParser;
     use std::fs;
 
     fn print_type_of<T>(_: &T) {
@@ -73,8 +116,8 @@ mod tests {
     #[test]
     fn it_worked() {
         // run with cargo test -- --nocapture
-        let input = fs::read_to_string("tests/test2.md").unwrap();
-        let mut parser: Parser = Parser::new();
+        let input = fs::read_to_string("tests/test3.md").unwrap();
+        let mut parser: BlockParser = BlockParser::new();
         parser.parse(&input);
         parser.show();
     }
