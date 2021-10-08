fd45746930d5637b4860b8ad0be0b39afb3f3ece
xiayulu <zuiaiqiansen@163.com>
2021-10-08 21:51:01 +0800

    finish

--- src/head.rs
+++ src/head.rs
@@ -1,22 +1,39 @@
 #![allow(unused)]
 
-#[derive(Debug)]
-struct Mark(String);
+use crate::inline::parse_inline;
+use crate::inline::Span;
 
 #[derive(Debug)]
-struct Head(String);
-
-#[derive(Debug)]
-pub struct HeadParser {
-    marks: Vec<Mark>,
-    heads: Vec<Head>,
+pub struct Heading {
+    level: usize,
+    spans: Vec<Span>,
 }
 
-impl HeadParser {
-    pub fn new() -> Self {
-        HeadParser {
-            marks: Vec::new(),
-            heads: Vec::new(),
-        }
+pub fn parse_heading(input: &String) -> Heading {
+    let head_marks = input.split_whitespace().next().unwrap_or("");
+    let head_body = input
+        .strip_prefix(&format!("{} ", head_marks))
+        .unwrap_or("");
+
+    let spans = parse_inline(&String::from(head_body));
+
+    Heading {
+        level: head_marks.len(),
+        spans: spans,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_heading;
+    use std::fs;
+
+    #[test]
+    fn test_inline() {
+        // run with cargo test -- --nocapture
+        let input = String::from("# hello $math$");
+
+        let head = parse_heading(&input);
+        println!("{:?}", head);
     }
 }
